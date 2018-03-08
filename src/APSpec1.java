f7704eee89199780b6a6cbabc13a005c632c9dcb
sp14 <kokorokikoro@gmail.com>
2018-03-08 07:05:23 +0000

    katastrofi. de douleyei tipota

--- src/APSpec1.java
+++ src/APSpec1.java
@@ -2,7 +2,6 @@
 public class APSpec1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Grid grid = new Grid();
 		VehicleGenerator vGen = new VehicleGenerator(grid);
 
